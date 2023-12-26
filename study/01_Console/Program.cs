f07dda7433f265d464d467d6c897810beb0fb071
yeolchiyang <yangchi97@naver.com>
2023-12-26 15:19:30 +0900

    C# 기초 입력출력

---
+++ study/01_Console/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _01_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Console.WriteLine("Hello, World!");
+            Console.Write("고고고고고\n노노노노노노");
+            Console.WriteLine("my name is 이재웅");
+
+            string input = Console.ReadLine(); //키볻 입력을 받아서 string 변수 input 에 저장하기
+
+            Console.WriteLine(input); //input 출력
+             */
+            int check = 0;
+            Console.WriteLine("당신의 이름을 입력하세요");
+            string nameInput = Console.ReadLine();
+            while (check < 3)
+            {
+                check++;
+                Console.WriteLine(nameInput);
+            }
+            
+        }
+    }
+}
